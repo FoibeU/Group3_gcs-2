87c8635c97f17a5c0bd6d71d9f2cf85ecc45f4e7
FoibeU <116794495+FoibeU@users.noreply.github.com>
2023-02-18 18:06:05 +0200

    Delete Shell_Script directory

--- Shell_Script/words_and_lines.sh
+++
@@ -1,6 +0,0 @@
-echo "Enter any sentence"
-read text
-word=$(echo -n"$text" |wc -w)
-echo "The number of words are : "$word
-space=$(echo $text | awk -F "" '{for(i=1;i<=NF;i++)if($i==" ")c++ }END{print c}')
-echo "The numbers of white spaces is: "$space 
