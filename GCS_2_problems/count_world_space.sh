a0202c6448e28b852f241e4e82218b428a38a03b
Guled12345 <117306990+Guled12345@users.noreply.github.com>
2023-02-18 22:10:53 +0200

    Update count_world_space.sh

--- GCS_2_problems/count_world_space.sh
+++ GCS_2_problems/count_world_space.sh
@@ -1,1 +1,6 @@
-
+echo "Enter any sentence "
+read text
+word=$(echo -n "$text" |wc -w|
+echo "The numberof the words : "$word
+space=$(echo $text | awk -F "" '{for (i=1;i<=NF;i++)if($i==" ")c++ }END{print c}')
+echo "The numbers of white space is :"$space 
